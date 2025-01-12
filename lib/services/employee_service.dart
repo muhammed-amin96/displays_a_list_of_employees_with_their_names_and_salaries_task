ad4bce1df248449acf725c1c666b172906dcb46f
Muhammad Amin <mohamedamin.fcih@gmail.com>
2025-01-12 16:21:35 +0200

    adjust employee service

--- lib/services/employee_service.dart
+++ lib/services/employee_service.dart
@@ -3,18 +3,18 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class EmployeeService {
-  var endPoint =
-      Uri.http('https://mocki.io/v1/283ba093-9bf9-42e4-8f28-d2538937f9ca');
-  List<Employee> employees = [];
+  String uri = 'https://mocki.io/v1/283ba093-9bf9-42e4-8f28-d2538937f9ca';
+
   Future<List<Employee>> getEmployees() async {
-    var response = await http.get(endPoint);
+    var response = await http.get(Uri.parse(uri));
+    List<Employee> employees = [];
     if (response.statusCode == 200) {
       var data = json.decode(response.body);
       data.forEach((element) {
         Employee employee = Employee.fromJson(element);
         employees.add(employee);
       });
-      print(employees);
+      print('Number of Employees is ${employees.length}');
     } else {
       throw Exception('Request failed with status: ${response.statusCode}.');
     }
