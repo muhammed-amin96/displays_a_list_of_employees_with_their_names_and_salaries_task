a7ae85e5cc7df90533bcc40c1fc19e75db8c2c71
Muhammad Amin <mohamedamin.fcih@gmail.com>
2025-01-12 16:50:59 +0200

    parse and cach data

--- lib/services/employee_service.dart
+++ lib/services/employee_service.dart
@@ -1,22 +1,28 @@
+import 'package:dio/dio.dart';
 import 'package:displays_a_list_of_employees_with_their_names_and_salaries_task/models/employee_model.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+import 'package:shared_preferences/shared_preferences.dart';
+
 class EmployeeService {
-  String uri = 'https://mocki.io/v1/283ba093-9bf9-42e4-8f28-d2538937f9ca';
+  String endPoint = 'https://mocki.io/v1/283ba093-9bf9-42e4-8f28-d2538937f9ca';
+  List<Employee> employees = [];
 
   Future<List<Employee>> getEmployees() async {
-    var response = await http.get(Uri.parse(uri));
-    List<Employee> employees = [];
-    if (response.statusCode == 200) {
-      var data = json.decode(response.body);
+    try {
+      var response = await Dio().get(endPoint);
+      var data = response.data; //parse response data
+      var cachedData = jsonEncode(data); //cconvert json to string
+      final prefs = await SharedPreferences.getInstance();
+      prefs.setString(
+          'employeesData', cachedData); //store data in shared preferences
       data.forEach((element) {
-        Employee employee = Employee.fromJson(element);
+        Employee employee = Employee.fromJson(element); //parse each user
         employees.add(employee);
       });
-      print('Number of Employees is ${employees.length}');
-    } else {
-      throw Exception('Request failed with status: ${response.statusCode}.');
+    } catch (e) {
+      print(e.toString());
     }
     return employees;
   }
